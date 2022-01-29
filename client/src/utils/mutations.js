52b293c2c54cb3e1280c51d2fd098ae2decbbdb5
Victor Smith <victorj500@gmail.com>
2022-01-29 17:54:37 -0600

    login works

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,12 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
-    loginUser(email: $email, password: $password) {
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
-        _id
         username
+        email
       }
     }
   }
