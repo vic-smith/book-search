65b6c114383da3e027567b1e541e010fe61fc10b
Victor Smith <victorj500@gmail.com>
2022-01-30 17:18:26 -0600

    SAVE_BOOK mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -72,9 +72,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const {data} = await saveBook({
+        variables: { book:{ ...bookToSave} },
+      });
 
-      if (!response.ok) {
+      if (!data.ok) {
         throw new Error('something went wrong!');
       }
 
