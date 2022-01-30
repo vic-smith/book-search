4dc3a4520dc545b23fa75eade776419a32c8151a
Victor Smith <victorj500@gmail.com>
2022-01-30 14:47:52 -0600

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require("mongoose")
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
@@ -7,9 +6,9 @@ const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/book-search";
+ const app = express();
+ const PORT = process.env.PORT || 3001;
+
 
 const server = new ApolloServer({
   typeDefs,
@@ -17,20 +16,12 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
-
 server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if we're in production, serve client/build as static asset
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
