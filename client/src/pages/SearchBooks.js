e391a5c4614c9ebf0a319ccc8780d0ff0c297386
Victor Smith <victorj500@gmail.com>
2022-01-30 09:53:01 -0600

    added engine version to package.json

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
-  const [saveBook] = useMutation(SAVE_BOOK);
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-  me {
-    _id
-    username
-    email 
-    bookCount
-    savedBooks{
-      bookId
-      authors
-      title
-      image
-      link
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        image
+        link
+      }
     }
   }
 `;
